fdee21ecf8e46d961fadc6f54007f4e67f916dc7
Ubuntu <oriree70.com>
2024-03-02 00:21:06 +0000

    15th commit

--- app.java
+++ app.java
@@ -2,3 +2,4 @@ We are thinking of writing the app in Jave.
 It might be difficult to get a developer for that though.
 Maybe but we will keep pushing.
 Making more changes to this file. I just hope it will be approved.
+it's getting
